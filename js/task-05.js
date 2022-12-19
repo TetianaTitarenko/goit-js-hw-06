498840342aa305f717f51703da208c51d0a7dd78
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2022-12-19 18:08:26 +0000

    corrected "Anonymous"

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,13 @@
 const input = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output")
+console.log(nameOutput)
+
 
 input.addEventListener("input", onName);
 
 function onName(event) {
-    nameOutput.textContent = event.currentTarget.value
+      nameOutput.textContent = event.currentTarget.value  
+    if (event.currentTarget.value === "") {
+        nameOutput.textContent = "Anonymous"
+    }
 }
